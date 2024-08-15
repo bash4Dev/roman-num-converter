d103a154cd3afab51b01fd0744187133d88a7a7e
Bashir Umar Idris <bashtrust15@gmail.com>
2024-08-15 21:26:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const convertBtn = document.getElementById("convert-btn");
 
 output.style.visibility = "hidden";
 
+const numValues = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
+]
+
 const convertNum = () => {
     output.style.visibility = "visible";
 
